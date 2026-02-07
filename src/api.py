d47d101807e349d6b9538ed2bec7df2431786266
Balaji J <j.balaji4200@gmail.com>
2026-02-07 23:51:36 +0530

    feat: connect router to app
    Signed-off-by: Balaji J <j.balaji4200@gmail.com>

--- src/api.py
+++ src/api.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from src.auth.controller import authRouter
+from src.files.controller import file_router
 
 def register_routes(app: FastAPI):
     app.include_router(authRouter)
+    app.include_router(file_router)
